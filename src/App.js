1ff673d5094edb8fac3498e9120d8074229d1f46
youguii <mourad-dehouche@hotmail.com>
2022-01-25 22:22:12 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+  const LOCAL_STORAGE_KEY = 'todoApp.todos';
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
   function handleAddTodo(e) {
     const name = todoNameRef.current.value;
     if (name === '') return;
     setTodos((prevTodos) => {
-      return [...prevTodos, { id: 1, name: name, complete: false }];
+      return [...prevTodos, { id: name, name: name, complete: false }];
     });
     todoNameRef.current.value = null;
   }
